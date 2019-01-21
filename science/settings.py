592982a7a730c3f5888aeef43418e18f436d67ca
jeankao <koma@mail.nksh.tp.edu.tw>
2019-01-21 15:50:04 +0800

    update

--- science/settings.py
+++ science/settings.py
@@ -78,8 +78,16 @@ WSGI_APPLICATION = 'science.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'science',
+    'USER': 'postgres',
+    'PASSWORD': '5543',
+    'HOST': 'localhost',
+    'PORT': '5432',
+
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
